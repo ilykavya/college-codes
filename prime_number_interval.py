b183f85306fde07f76be50b1c953d1e2c6d092c5
ilykavya <25l31a43g6@gmail.com>
2026-08-30 05:24:50 +0000

    Correct prime number interval logic and syntax
    Fix syntax errors and improve prime number calculation.

---
+++ prime_number_interval.py
@@ -0,0 +1,24 @@
+lower=int(input("enter lower limit:"))
+upper=int(input("enter upper limit:"))
+print(f"\nprime numbers between {lower} and {upper} are:")
+print("-"*40)
+for num in range(lower,upper+1):
+  if num<=1;
+  contiue
+  is_prime=true
+  for i in range(2,int(num**0.5)=1):
+    if num%i==0:
+      is_prime=false
+      break
+      if is_prime:
+        print(num,end=" ")
+        print("\n"+"-"*40)
+
+
+
+#sample output:
+#enter lower limit:10
+#enter upper limit:50
+#
+#prime numbers between 10 and 50 are:
+#11 13 17 19 23 29 31 41 43 47
